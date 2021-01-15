c89f210c63b1ce588ac69fdece4ff02da8f010d3
Neolau <ulrick.neola@hotmail.fr>
2021-01-15 18:08:51 +0000

    #add content handler

---
+++ src/components/menu/handler-content.composent.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+import ContactContent from "../../contents/contacts/contactsContent"
+import DiplomeContent from "../../contents/diplome/diplomeContent"
+import HistoricContent from "../../contents/historic/historicContent"
+import HomeContent from "../../contents/home/homeContent"
+import CoursContent from "../../contents/service/coursContent"
+import CoutureArtisanalContent from "../../contents/service/coutureArtisanalContent"
+import CouturePersonaliseContent from "../../contents/service/couturePesroContent"
+import RetouchesContent from "../../contents/service/retouchesContent"
+import TransfoVetementsContent from "../../contents/service/transfoContent"
+
+type HandlerContentCompoProps = {
+  isHome: boolean
+  isContacts: boolean
+  isDiplome: boolean
+  isHistoric: boolean
+  isTransfo: boolean
+  isCours: boolean
+  isCoutureArti: boolean
+  isCouturePesro: boolean
+  isRetouches: boolean
+}
+
+const HandlerContentComposent: React.FC<HandlerContentCompoProps> = ({
+  isHome,
+  isContacts,
+  isDiplome,
+  isHistoric,
+  isTransfo,
+  isCours,
+  isCoutureArti,
+  isCouturePesro,
+  isRetouches
+}) => {
+
+  return (
+    <>
+      {isHome && <HomeContent />}
+      {isContacts && <ContactContent />}
+      {isDiplome && <DiplomeContent />}
+      {isHistoric && <HistoricContent />}
+      {isTransfo && <TransfoVetementsContent />}
+      {isCours && <CoursContent />}
+      {isCoutureArti && <CoutureArtisanalContent />}
+      {isCouturePesro && <CouturePersonaliseContent />}
+      {isRetouches && <RetouchesContent />}
+    </>)
+}
+
+export default React.memo(HandlerContentComposent)
--- src/mainPage.tsx
+++ src/mainPage.tsx
@@ -1,12 +1,9 @@
 import { Divider } from "antd";
 import Layout, { Content } from "antd/lib/layout/layout";
 import React, { useState } from "react";
+import HandlerContentComposent from "./components/menu/handler-content.composent";
 import HeaderMenuComponent from "./components/menu/header-menu.component";
 import SideMenuComponent from "./components/menu/side-menu.component";
-import ContactContent from "./contents/contacts/contactsContent";
-import DiplomeContent from "./contents/diplome/diplomeContent";
-import HistoricContent from "./contents/historic/historicContent";
-import HomeContent from "./contents/home/homeContent";
 import './mainPage.css'
 
 
@@ -17,12 +14,22 @@ function MainPage() {
   const [isContacts, setIsContacts] = useState(false)
   const [isDiplome, setIsDiplome] = useState(false)
   const [isHistoric, setIsHistoric] = useState(false)
+  const [isTransfo, setIsTransfo] = useState(false)
+  const [isCours, setIsCours] = useState(false)
+  const [isCoutureArti, setIsCoutureArti] = useState(false)
+  const [isCouturePesro, setIsCouturePesro] = useState(false)
+  const [isRetouches, setIsRetouches] = useState(false)
 
   const handleContent = (key: string) => {
     setIsHome(false)
     setIsContacts(false)
     setIsDiplome(false)
     setIsHistoric(false)
+    setIsTransfo(false)
+    setIsCours(false)
+    setIsCoutureArti(false)
+    setIsCouturePesro(false)
+    setIsRetouches(false)
 
     switch (key) {
       case 'Home':
@@ -36,6 +43,21 @@ function MainPage() {
         break
       case 'Historic':
         setIsHistoric(true)
+        break
+      case 'transfo':
+        setIsTransfo(true)
+        break
+      case 'couturePerso':
+        setIsCouturePesro(true)
+        break
+      case 'retouches':
+        setIsRetouches(true)
+        break
+      case 'coutureArtisa':
+        setIsCoutureArti(true)
+        break
+      case 'cours':
+        setIsCours(true)
         break
       default:
         setIsHome(true)
@@ -57,10 +79,17 @@ function MainPage() {
                 minHeight: 280,
               }}
             >
-              {isHome && <HomeContent />}
-              {isContacts && <ContactContent />}
-              {isDiplome && <DiplomeContent />}
-              {isHistoric && <HistoricContent />}
+              {<HandlerContentComposent
+                isHome={isHome}
+                isContacts={isContacts}
+                isDiplome={isDiplome}
+                isHistoric={isHistoric}
+                isTransfo={isTransfo}
+                isCours={isCours}
+                isCoutureArti={isCoutureArti}
+                isCouturePesro={isCouturePesro}
+                isRetouches={isRetouches}
+              />}
             </Content>
           </div>
           <Divider plain>
