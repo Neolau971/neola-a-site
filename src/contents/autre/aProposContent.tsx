2a874964680a81e1d5fe0e039790af8c4b1b53c3
Neolau <ulrick.neola@hotmail.fr>
2021-01-19 08:58:29 +0000

    #update a propos content

--- src/contents/autre/aProposContent.tsx
+++ src/contents/autre/aProposContent.tsx
@@ -1,11 +1,12 @@
-import { Divider } from "antd"
+import { Divider, Row, Col } from "antd"
 import React from "react"
+import profile from '../../pictures/profile.jpg'
 
-const text1 = "Le CEO et Créatrice De Mode"
-const text2 = "après avoir étudié dans l'école de haute couture de Toulouse en 2004,"
-const text3 = "à lancé Alietta création en 2011."
-const text4 = "Cette entreprise qui évolue dans l'industrie de l'habillement"
-const text5 = "à su évolué et trouvé ça place dans la société guadeloupéenne et compte de nombreux clients satisfait."
+const text1 = "Le CEO et Créatrice De Mode après avoir étudié dans l'école de haute"
+const text2 = "couture de Toulouse en 2004, à lancé Alietta création en 2011."
+const text3 = "Cette entreprise qui évolue dans l'industrie de l'habillement"
+const text4 = "à su évolué et trouvé ça place dans la société guadeloupéenne et compte"
+const text5 = "de nombreux clients satisfait."
 
 const styles = {
   margin: '20px',
@@ -17,13 +18,24 @@ function AProposContent() {
     <div className="AppText">
       <Divider plain><div className="AppText" style={{ fontSize: 'x-large' }}>À propos</div></Divider>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <p>
-          <div style={styles}>{text1}</div>
-          <div style={styles}>{text2}</div>
-          <div style={styles}>{text3}</div>
-          <div style={styles}>{text4}</div>
-          <div style={styles}>{text5}</div>
-        </p>
+        <Row>
+          <Col>
+            <div style={{ marginRight: '100px' }}>
+              <img src={profile} alt="profile" />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>
+              <div style={styles}>{text1}</div>
+              <div style={styles}>{text2}</div>
+              <div style={styles}>{text3}</div>
+              <div style={styles}>{text4}</div>
+              <div style={styles}>{text5}</div>
+            </p>
+          </Col>
+        </Row>
       </div>
     </div>
   )
